8d860fd1618e990b1915d2ec452aba42833dd479
Kritika3861 <124664792+Kritika3861@users.noreply.github.com>
2023-02-14 13:37:48 +0530

    Add files via upload

---
+++ linear.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int linear(int [],int ,int);
+int linear(int arr[],int n,int x)
+{
+   int i;
+   for(i=0;i<n;i++)
+   {
+     if(arr[i]==x)
+       return i;
+     else
+       return -1;
+   }
+}
+int main()
+{
+  int arr[100],i,n,x,a;
+  printf("Enter number of non negative integers ;");
+  scanf("%d",&n);
+  printf("Enter non negative integers :");
+  for(i=0;i<n;i++)
+  {
+     scanf("%d",&arr[i]); 
+  }
+  printf("Enter key");
+  scanf("%d",&x);
+  a=linear(arr,n,x);
+  printf("Key found at index %d",a);
+  return 0;
+}
