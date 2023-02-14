6e4d7f7acad554a71e795ed21d4499ccc5e535af
Kritika3861 <124664792+Kritika3861@users.noreply.github.com>
2023-02-14 14:03:08 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int binary(int [],int ,int,int );
+int binary(int arr[],int lb,int ub,int k)
+{
+  int mid;
+  mid=(lb+ub)/2;
+  if(arr[mid]==k)
+    return mid;
+  else if(arr[mid]>k)
+    return binary(arr,lb,mid-1,k);
+  else
+    return binary(arr,mid+1,ub,k);
+    
+}
+int main()
+{
+  int arr[100],i,n,k,a,lb,ub;
+  printf("Enter number of non negative integers :");
+  scanf("%d",&n);
+  lb=0;
+  ub=n-1;
+  printf("Enter sorted positive integers :");
+  for(i=0;i<n;i++)
+  {
+     scanf("%d",&arr[i]); 
+  }
+  printf("Enter key");
+  scanf("%d",&k);
+  a=binary(arr,lb,ub,k);
+  printf("Key found at index %d",a);
+  return 0;
+}
+  
